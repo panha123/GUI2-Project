1054f7a2a8683452f848a49d9be87c8935af140f
kevintran1308 <44125926+kevintran1308@users.noreply.github.com>
2019-03-24 11:40:48 -0500

    added Dashboard3

--- stockportfolio/src/App.js
+++ stockportfolio/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import Dashboard from "./components/dashboard/Dashboard";
+import Dashboard3 from "./components/dashboard/Dashboard3";
 import ProjectDetails from "./components/projects/ProjectDetails";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
@@ -15,7 +15,7 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Switch>
-            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/" component={Dashboard3} />
             <Route path="/project/:id" component={ProjectDetails} />
             <Route path="/signin" component={SignIn} />
             <Route path="/signup" component={SignUp} />
