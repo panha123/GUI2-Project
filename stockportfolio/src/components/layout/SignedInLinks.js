ed087fe884ab4340189cbcfde1e3b084ed178e32
Marittyakeu <Marittya_keu@student.uml.edu>
2019-03-24 13:24:43 -0400

    Added route to profile

--- stockportfolio/src/components/layout/SignedInLinks.js
+++ stockportfolio/src/components/layout/SignedInLinks.js
@@ -10,8 +10,9 @@ const SignedInLinks = (props) => {
     <div>
       <ul className="right">
         <li><NavLink to='/createproject'>New Project</NavLink></li>
-        <li><a onClick={props.signOut}>Sign Out</a> </li>
+        <li><NavLink to='/profile'>Profile</NavLink></li>
         <li><NavLink to='/' className="btn btn-floating pink lighten-1">MK</NavLink></li>
+        <li><a onClick={props.signOut}>Sign Out</a> </li>
       </ul>
     </div>
   )
