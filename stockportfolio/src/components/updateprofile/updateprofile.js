500b156e1bab4aa174ab71a6feb8023fc5a6b890
van_ha@student.uml.edu <van_ha@student.uml.edu>
2019-03-24 14:12:29 -0400

    export default

--- stockportfolio/src/components/updateprofile/updateprofile.js
+++ stockportfolio/src/components/updateprofile/updateprofile.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
-export class UpdateProfile extends Component {
+export default class UpdateProfile extends Component {
     render() {
         return (
         <div className="container">
