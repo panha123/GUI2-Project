415749568d132803416b436675f7b948284e0034
Marittyakeu <Marittya_keu@student.uml.edu>
2019-03-24 17:59:45 -0400

    fetch testing

--- stockportfolio/src/components/dashboard/Dashboard2.js
+++ stockportfolio/src/components/dashboard/Dashboard2.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
@@ -27,33 +26,6 @@ export class Dashboard2 extends Component {
         <div className="col m5">
         </div>
 
-=======
-import React, { Component } from 'react'
-import { firestoreConnect } from "react-redux-firebase";
-import { compose } from "redux";
-import { Redirect } from "react-router-dom";
-import { connect } from "react-redux";
-
-export class Dashboard2 extends Component {
-  render() {
-    const { projects, auth } = this.props;
-      if (!auth.uid) {
-        return <Redirect to="/signin" />;
-      }
-      return (
-        <div className="dashboard row main-dash">
-          {/* 
-            left side dashboard
-            - user avatar
-            - transaction data
-            - equity
-            - YTD Gains
-            - YTD Taxes
-          */}
-          <h1>TEST</h1>
-          <div className="col m5">
-          </div>
->>>>>>> 04663b8dfcaa4d47a14a29323d2cb60ea79ab9e8
 
 
 
@@ -127,10 +99,6 @@ export class Dashboard2 extends Component {
       )
   }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 04663b8dfcaa4d47a14a29323d2cb60ea79ab9e8
 const mapStateToProps = state => {
   console.log(state);
   return {
@@ -141,8 +109,4 @@ const mapStateToProps = state => {
 export default compose(
   connect(mapStateToProps),
   firestoreConnect([{ collection: "projects" }])
-<<<<<<< HEAD
 )(Dashboard2);
-=======
-)(Dashboard2);
->>>>>>> 04663b8dfcaa4d47a14a29323d2cb60ea79ab9e8
--- stockportfolio/src/store/actions/FetchAction.js
+++ stockportfolio/src/store/actions/FetchAction.js
@@ -1,4 +1,4 @@
-export const fetchUserProfile = () => {
+export const fetchUserProfile = (state) => {
     return(dispatch, getState, { getFirestore }) => {
         const firestore = getFirestore();
         const authorId = getState().firebase.auth.uid;
@@ -6,9 +6,8 @@ export const fetchUserProfile = () => {
         
         firestore.collection('user').doc(authorId).get()
         .then((resp) => {
-            // console.log(typeof resp.data());
-            // return resp.data();
-            
+            console.log(typeof resp.data());
+
         })
         .catch( err => {
             console.log(err);
