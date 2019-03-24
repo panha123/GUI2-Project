682c5d73fac9cc9c23b08c4ede072cbd7a01e886
van_ha@student.uml.edu <van_ha@student.uml.edu>
2019-03-24 14:09:52 -0400

    updateprofile

--- stockportfolio/src/App.js
+++ stockportfolio/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-// import Dashboard3 from "./components/dashboard/Dashboard3";
+import Dashboard2 from "./components/dashboard/Dashboard2";
 import ProjectDetails from "./components/projects/ProjectDetails";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
@@ -10,7 +10,8 @@ import UpdateProfile from "./components/updateprofile/updateprofile"
 import ForgetPassword from "./components/auth/ForgetPassword";
 
 class App extends Component {
-    return (
+      render() {
+        return (
       <BrowserRouter>
         <div className="App">
           <Navbar />
@@ -24,12 +25,13 @@ class App extends Component {
             <Route path="/createproject" component={CreateProject} />
             <Route path="/updateprofile" component={UpdateProfile} />
             <Route path="/ForgetPassword" component={ForgetPassword} />
-  
           </Switch>
         </div>
       </BrowserRouter>
     );
+   }
   }
-}
 
+
+  
 export default App;
--- stockportfolio/src/components/updateprofile/updateprofile.js
+++ stockportfolio/src/components/updateprofile/updateprofile.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
-class UpdateProfile extends Component {
+export class UpdateProfile extends Component {
     render() {
         return (
         <div className="container">
