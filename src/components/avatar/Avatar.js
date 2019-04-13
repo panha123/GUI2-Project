89cf5b8908d1bfb7f50984871aa54edfa4850b86
kevintran1308 <44125926+kevintran1308@users.noreply.github.com>
2019-04-13 17:23:40 -0500

    upload image

--- src/components/avatar/Avatar.js
+++ src/components/avatar/Avatar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import firebase from 'firebase/app';
 import Pika from '../../img/pika.jpg';
-
+import { Redirect } from "react-router-dom";
 
 export class Avatar extends Component {
     state = {
@@ -10,9 +10,19 @@ export class Avatar extends Component {
         email: '',
         income: '',
         filingstatus: '',
-        dependents: ''
+        dependents: '',
+        redirect: false
     }
-
+    setRedirect = () => {
+        this.setState({
+          redirect: true
+        })
+    }
+    renderRedirect = () => {
+        if (this.state.redirect) {
+          return <Redirect to='/updateprofile' />
+        }
+      }
     componentDidMount = () => {
         const db = firebase.firestore();
         const docRef = db.collection('user').doc(firebase.auth().currentUser.uid);
@@ -28,10 +38,15 @@ export class Avatar extends Component {
     }
 
     render() {
+        
         return (
             <div className="ProfPic center">
             <img src={Pika} alt="Pika" width="200" height="200"/>
             <br/>
+            <div>
+                {this.renderRedirect()}
+                <button onClick={this.setRedirect}>Update Profile Picture</button>
+            </div>
             <span id="UserName center">{this.state.email}</span>
           </div>
         )
